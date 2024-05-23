6969363418a1f000ebc931eb9b27eec0a15c18aa
TristanRivaldi <145669069+TristanRivaldi@users.noreply.github.com>
2024-05-23 12:36:54 +0200

    Modife pour que ca marche pour CIFAR10

--- Code_Experience/dataloader.py
+++ Code_Experience/dataloader.py
@@ -19,18 +19,18 @@ def get_dataloader(dataset_str, random_seed=0, train_ratio=0.8, batch_size_train
     print(f"Chargement du jeu de données {dataset_str}...")
 
     if dataset_str == 'cifar10':
-        # cf https://gist.github.com/weiaicunzai/e623931921efefd4c331622c344d8151 for normalization
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-        dataset_train = datasets.CIFAR10(root='Datasets/CIFAR10', train=True, transform=transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomCrop(32, 4),
-            transforms.ToTensor(),
-            normalize,
-        ]), download=True)
-        dataset_train, dataset_validation = torch.utils.data.random_split(
-            dataset_train, [train_ratio, 1.0 - train_ratio], generator=torch.Generator().manual_seed(random_seed))
-        dataset_test = torchvision.datasets.CIFAR10(root='Datasets/CIFAR10', train=False, download=True,
-                                                    transform=transforms.Compose([transforms.ToTensor(), normalize]))
+        cifar_mean = (0.4914, 0.4822, 0.4465)
+        cifar_std = (0.2023, 0.1994, 0.2010)
+        t = torchvision.transforms.Compose(
+            [torchvision.transforms.ToTensor(), torchvision.transforms.Normalize(cifar_mean, cifar_std)])
+        dataset_train = torchvision.datasets.CIFAR10(root='Datasets/CIFAR10', train=True, download=True, transform=t)
+        # NOTE : We will split the test set into validation / test because we want to use pretrained models that will be used on the whole training set
+        dataset_test = torchvision.datasets.CIFAR10(root='Datasets/CIFAR10', train=False, download=True, transform=t)
+        # NOTE : here train_ratio will be the size of the validation set (not very pretty)
+        dataset_validation, dataset_test = torch.utils.data.random_split(
+            dataset_test, [train_ratio, 1.0 - train_ratio], generator=torch.Generator().manual_seed(random_seed))
+
+        
 
     elif dataset_str == 'cifar100':
         cifar_mean = (0.5071, 0.4867, 0.4408)
